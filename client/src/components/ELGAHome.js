16c6cc3818d848ce2e43fc685ff6c83c195c96ad
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-05-20 22:24:51 +0100

    added gradient background

--- client/src/components/ELGAHome.js
+++ client/src/components/ELGAHome.js
@@ -3,10 +3,10 @@ import "./ELGAHome.css";
 
 const ELGAHome = () => {
   return (  
-    <>
+    <div className="bounce">
       <h1 className="title">ELGA</h1>
-      <img className="animate hidden delay-2s" id='welcome-egg' src={require("../images/spotted_egg.png")} alt="polka dot easter egg"></img>
-    </>
+      <img className="animate" id='welcome-egg' src={require("../images/spotted_egg.png")} alt="polka dot easter egg"></img>
+    </div>
   );
 }
  
