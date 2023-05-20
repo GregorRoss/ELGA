7a20491880a7cf8961f5a97cbff324119c00e639
Ross Aitken <raaitken@gmail.com>
2023-05-20 12:50:10 +0100

    Added basic routes for three components

---
+++ client/src/components/Images.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Images = () => {
+    return (  
+        <>
+      <h1>Images</h1>
+        </>
+    );
+}
+ 
+export default Images;
---
+++ client/src/components/Phrases.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Phrases = () => {
+    return (  
+        <>
+            <h1>Phrases</h1>
+        </>
+    );
+}
+ 
+export default Phrases;
