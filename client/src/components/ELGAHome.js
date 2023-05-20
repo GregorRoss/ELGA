72507ec5a118989c31dc4fa848e2e84dc7218513
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-05-20 19:34:33 +0100

    added styling to home Screen

--- client/src/components/ELGAHome.js
+++ client/src/components/ELGAHome.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./ELGAHome.css";
 
 const ELGAHome = () => {
   return (  
     <>
-      <h1>Hola, Mundo!</h1>
+      <h1 className="title">ELGA</h1>
+      <img className="animate hidden delay-2s" id='welcome-egg' src={require("../images/spotted_egg.png")} alt="polka dot easter egg"></img>
     </>
   );
 }
