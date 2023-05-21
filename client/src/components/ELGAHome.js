42c1ea3b52f06efcd34bbce255bb19b4168ee91b
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-05-21 14:36:52 +0100

    welcome page styling complete

--- client/src/components/ELGAHome.js
+++ client/src/components/ELGAHome.js
@@ -3,10 +3,14 @@ import "./ELGAHome.css";
 
 const ELGAHome = () => {
   return (  
-    <div className="bounce">
+    <>
       <h1 className="title">ELGA</h1>
-      <img className="animate" id='welcome-egg' src={require("../images/spotted_egg.png")} alt="polka dot easter egg"></img>
-    </div>
+      {/* seperate div in order to apply second animation to image */}
+      <div className="bounce">
+        <img className="slide-in-left shadow" id='welcome-egg' src={require("../images/spotted_egg.png")} alt="polka dot easter egg"></img>
+      </div>
+      <p className="tap-indicator pulse">tap anywhere</p>
+    </>
   );
 }
  
