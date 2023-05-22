f4be941bcbe77d998eec554205a2c7771a3f2b10
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-05-22 11:32:51 +0100

    adding links to games

--- client/src/components/ELGASplash.js
+++ client/src/components/ELGASplash.js
@@ -5,7 +5,7 @@ import "./ELGASplash.css";
 const ELGAHome = ({handleHomeClick, clicked}) => {
   return (  
     <Link to="/home" className="home-container" onClick={handleHomeClick} clicked={clicked}>
-      <h1 className="title">ELGA</h1>
+      <h1 className="elga-title">ELGA</h1>
       {/* seperate div in order to apply second animation to image */}
       <div className="bounce">
         <img className="slide-in-left shadow" id='welcome-egg' src={require("../images/spotted_egg.png")} alt="polka dot easter egg"></img>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Home.css"
+
 const Home = () => {
     return ( 
-        <>
-        <p>home</p>
-        </>
+        <div className="home-container">
+            <h1 className="home-title">Quieres jugar?</h1>
+            <Link to="/numbers" className="grow">
+                <h2 className="game-title">numbers</h2>
+                <img className="game-image" src={require("../images/calculator.png")} alt="calculator image"></img>
+            </Link>
+            <Link to="/images">
+                <h2 className="game-title">images</h2>
+                <img className="game-image" src={require("../images/images.png")} alt="pictures image"></img>
+            </Link>
+            <Link to="/phrases">
+                <h2 className="game-title">phrases</h2>
+                <img className="game-image" src={require("../images/question-sign.png")} alt="speech bubble image"></img>
+            </Link>
+        </div>
      );
 }
  
